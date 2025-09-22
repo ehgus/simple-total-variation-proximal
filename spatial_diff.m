3a60caa8d728f571c04d242b64a7e2787bcaca6b
Dohyeon Lee <dleh428@gmail.com>
2025-09-22 16:16:41 +0900

    Use circular diff for efficiency calculation

--- spatial_diff.m
+++ spatial_diff.m
@@ -9,6 +9,11 @@ function out_arr = spatial_diff(out_arr, in_arr)
     expected_out_arr_dims = horzcat(ndims_in, in_arr_dims);
     assert(all(expected_out_arr_dims == size(out_arr)), "Size of arrays are not valid")
 
+    % Initialize out_arr
+    for dim = 1:ndims_in
+        out_arr(dim,:) = in_arr(:);
+    end
+
     % Compute partial differences along each dimension
     for dim = 1:ndims_in
         new_in_dims = [1 prod(in_arr_dims(1:dim-1)) in_arr_dims(dim) prod(in_arr_dims(dim+1:end))];
@@ -17,10 +22,8 @@ function out_arr = spatial_diff(out_arr, in_arr)
 
         in_arr = reshape(in_arr, new_in_dims);
         out_arr = reshape(out_arr, new_out_dims);
-        % Initialize staring edges zero values.
-        out_arr(dim,:,1,:) = 0;
         % Take centered differentiation
-        out_arr(dim,:,2:end,:) = in_arr(1,:,2:end,:) - in_arr(1,:,1:end-1,:);
+        out_arr(dim,:,:,:) = out_arr(dim,:,:,:) - circshift(in_arr, -1, 3);
     end
     out_arr = reshape(out_arr, expected_out_arr_dims);
 end
--- spatial_diff_T.m
+++ spatial_diff_T.m
@@ -10,7 +10,7 @@ function out_arr = spatial_diff_T(out_arr, in_arr)
     assert(all(expected_in_arr_dims == size(in_arr)), "Size of arrays are not valid")
 
     % Initialize out_arr
-    out_arr(:) = 0;
+    out_arr(:) = sum(in_arr,1);
 
     % Compute partial differences along each dimension
     for dim = 1:ndims_out
@@ -21,7 +21,7 @@ function out_arr = spatial_diff_T(out_arr, in_arr)
         out_arr = reshape(out_arr, new_out_dims);
         in_arr = reshape(in_arr, new_in_dims);
         % Take centered differentiation
-        out_arr(1,:,1:end-1,:) = out_arr(1,:,1:end-1,:) + in_arr(dim,:,1:end-1,:) - in_arr(dim,:,2:end,:);
+        out_arr = out_arr - circshift(in_arr(dim,:,:,:), 1, 3);
     end
     out_arr = reshape(out_arr, out_arr_dims);
 end
